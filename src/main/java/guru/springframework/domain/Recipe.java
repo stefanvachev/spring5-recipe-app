380836a7862fc9d1aa6770152d6629eb9cd4509b
stefanvachev <vatchev@prodigy.net>
2020-08-09 16:44:37 -0500

    Added Repositories and complete JPA Mappings.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -26,8 +26,29 @@ public class Recipe {
     @Lob //Will create blob  for non String types, if type is string, then clob
     private Byte[] image;
 
+    //EnumType Ordinal, which is default, will persist as 1,2,3 - an index representing the way the enums are ordered in the class.
+    //This is dangerous if you insert another enum in the class and its not the last one, existing indexes can break!
+    //EnumType.STRING will get the name, and store that in the DB, much safer.
+    @Enumerated(value = EnumType.STRING)
+    private Difficulty difficulty;
+
     @OneToOne(cascade = CascadeType.ALL)
     private Notes notes;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"), //from this side of the relationship, this is the field
+            inverseJoinColumns = @JoinColumn(name = "category_id") //from the other side of the relationship, this is the column.
+    )
+    private Set<Category> categories;
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 
     public Long getId() {
         return id;
@@ -115,6 +136,14 @@ public class Recipe {
 
     public void setNotes(Notes notes) {
         this.notes = notes;
+    }
+
+    public Difficulty getDifficulty() {
+        return difficulty;
+    }
+
+    public void setDifficulty(Difficulty difficulty) {
+        this.difficulty = difficulty;
     }
 }
 
